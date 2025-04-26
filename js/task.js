4a9364c7250ef1ff04e4260ac36084d48c300367
Adrià <adriabonnin@paucasesnovescifp.cat>
2025-04-26 19:55:50 +0200

    Crear nueva tarea y mas reponsive nav

---
+++ js/task.js
@@ -0,0 +1,11 @@
+class Task{
+    constructor(id, titol, descripcio, data, categoria, prioritat, realitzada){
+        this.id = id;
+        this.titol = titol;
+        this.descripcio = descripcio;
+        this.data = data;
+        this.categoria = categoria;
+        this.prioritat = prioritat;
+        this.realitzada = realitzada;
+    }
+}
