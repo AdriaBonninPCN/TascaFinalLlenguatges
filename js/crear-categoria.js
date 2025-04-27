f8c1fd924d82896589dc7fd7011062dd82278b39
Adrià <adriabonnin@paucasesnovescifp.cat>
2025-04-27 20:03:40 +0200

    CRUD categorias y icono

--- js/crear-categoria.js
+++ js/crear-categoria.js
@@ -1,20 +1,47 @@
 import { categories } from './database.js';
 import {categoria} from './categoria.js';
 
+let categoriesLocal = localStorage.getItem("categories");
+if (categoriesLocal){
+    JSON.parse(categoriesLocal).forEach(cate => {
+        categories.push(new categoria(cate.nom, cate.color))
+    });
+    console.log("categories carregades")
+    console.log(categories)
+}
+
 function crearCategoria(){
     let nom = document.getElementById("nom").value;
     let color = document.getElementById("color").value;
 
-    if((nom != null || nom != "") && (color != null || color != "")){
-        categories.push(new categoria(nom, color));
-        console.log(categories);
-        document.getElementById("nom").value = "";
-        document.getElementById("color").value = "";
-        console.log(categories)
+    let categoriaDuplicada = false;
+
+    //no se van a poder añadir 2 categorias con el mismo nombre :)
+    categories.forEach(cate => {
+        if (cate.nom == nom){
+            categoriaDuplicada = true;
+            console.log("categoriaduplicada")
+        }
+    })
+
+    if(!categoriaDuplicada){
+        if((nom != null && nom != "") && (color != null && color != "")){
+            categories.push(new categoria(nom, color));
+            document.getElementById("nom").value = "";
+            document.getElementById("color").value = "";
+            localStorage.setItem('categories', JSON.stringify(categories));
+            console.log(categories);
+        }
+        else{
+            console.log("error al crear categoria");
+            alert("revisa el nombre y el color");
+        }
     }
     else{
-        console.log("error")
+        alert("no puede haber 2 categorias con el mismo nombre");
     }
+
+
 }
 
 window.crearCategoria = crearCategoria;
