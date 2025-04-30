c1308faf20ebe50777ddd27ad26e5cdc1d5b93f1
Adrià <adriabonnin@paucasesnovescifp.cat>
2025-04-30 17:32:18 +0200

    cargar categorias en pagina tareas

--- js/crear-tarea.js
+++ js/crear-tarea.js
@@ -1,10 +1,11 @@
-import { categories, incrementaTasquesID } from './database.js';
+import { categories } from './database.js';
 import { tasques } from './database.js';
-import { tasquesID } from './database.js';
+import { tasquesID, incrementaTasquesID } from './database.js';
 import { task } from './task.js';
+import {categoria} from './categoria.js';
 
 let tareasLocal = localStorage.getItem("tareas");
-if(tareasLocal){
+if (tareasLocal) {
     JSON.parse(tareasLocal).forEach(tar => {
         tasques.push(new task(tar.id, tar.titol, tar.descripcio, tar.data, tar.categoria, tar.prioritat, tar.realitzada))
         incrementaTasquesID();
@@ -13,7 +14,7 @@ if(tareasLocal){
     console.log(tasques);
 }
 
-function crearTarea(){
+function crearTarea() {
     let id = tasquesID;
     let titol = document.getElementById("titol").value;
     let descripcio = document.getElementById("descripcio").value;
@@ -25,19 +26,20 @@ function crearTarea(){
     let tascaDuplicada = false;
 
     tasques.forEach(tar => {
-        if (tar.tiol == titol){
+        if (tar.tiol == titol) {
             tascaDuplicada = true;
-            console.log("categoria duplicada");
+            console.log("tarea duplicada");
         }
     });
 
-    if(!tascaDuplicada){
+    if (!tascaDuplicada) {
         tasques.push(new task(id, titol, descripcio, data, categoria, prioritat, realitzada));
         document.getElementById("titol").value = "";
         document.getElementById("descripcio").value = "";
         document.getElementById("data").value = "";
         document.getElementById("categoria").value = "";
         document.getElementById("prioritat").value = "";
+        localStorage.setItem("tareas", JSON.stringify(tasques));
         incrementaTasquesID();
         console.log("tarea creada");
     }
@@ -46,9 +48,54 @@ function crearTarea(){
         alert("revisa los datos introducidos, no puede haber 2 tareas con el mismo nombre");
     }
 
-    //cargarTareas();
+    cargarTareas();
+}
+
+function cargarTareas() {
+    document.getElementById("tareasCaja").innerHTML =
+        `
+        <h2>Tareas</h2>
+        `
+    tasques.forEach((element, index) => {
+        document.getElementById("tareasCaja").innerHTML +=
+            `
+            <div class="tarea-box">
+                <p class = "titulo">${element.titol}</p>
+                <p class = "descripcion">${element.descripcio}</p>
+                <p class = "fecha">${element.data}</p>
+                <p class = "categoria">${element.categoria}</p>
+                <p class = "prioridad">${element.prioritat}</p>
+                <p class = "realitzada">${element.realitzada}</p>
+            </div>
+            `
+    });
+
+    categories.forEach((element, index) => {
+        document.getElementById(index).style.backgroundColor = element.color;
+    });
+}
+
+function cargarCategoriasTareas() {
+    let categoriesLocal = localStorage.getItem("categories");
+    if (categoriesLocal) {
+        JSON.parse(categoriesLocal).forEach(cate => {
+            categories.push(new categoria(cate.nom, cate.color))
+        });
+        console.log("categories carregades");
+        console.log(categories);
+    }
+    categories.forEach((element) => {
+        document.getElementById("categoria").innerHTML +=
+            `
+                <option value="${element.color}">${element.nom}</option>
+            `
+    });
 }
 
 window.crearTarea = crearTarea;
 
 console.log("helo")
+
+cargarTareas();
+
+cargarCategoriasTareas();
